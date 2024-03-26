8d55e78bf7e59546b09cc25a37d85190a0dbe0ff
jang <treous11@gmail.com>
2024-03-26 12:45:02 +0900

    init

---
+++ src/quiz/Quiz2.java
@@ -0,0 +1,17 @@
+package quiz;
+
+public class Quiz2 {
+
+	public static void main(String[] args) {
+
+		int x = 10; // 11
+		int y = 20; // 19
+		
+		int result = (x++) + (--y); // 29
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(result);
+	}
+
+}
